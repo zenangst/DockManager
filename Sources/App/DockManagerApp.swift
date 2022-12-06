9daaf8768ca4017aed3624d6638bee09d58d940d
Christoffer Winterkvist <christoffer@winterkvist.com>
2022-12-06 07:32:46 +0100

    Add Reveal button in MenuBarExtra

--- Sources/App/DockManagerApp.swift
+++ Sources/App/DockManagerApp.swift
@@ -4,6 +4,11 @@ import SwiftUI
 struct DockManagerApp: App {
   var body: some Scene {
     MenuBarExtra(content: {
+      Button("Reveal", action: {
+        NSWorkspace.shared.selectFile(Bundle.main.bundlePath,
+                                      inFileViewerRootedAtPath: "")
+      })
+      Divider()
       Button("Quit", action: { NSApplication.shared.terminate(nil) })
     }, label: {
       Image(systemName: "dock.rectangle")
